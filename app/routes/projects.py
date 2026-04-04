bda4260b53c42f93f4e937caaa0870e8a23ae504
Caitlin-Fogg <caitlinfogg01@gmail.com>
2026-04-04 22:47:42 +0200

    CRUD bug fix

--- app/routes/projects.py
+++ app/routes/projects.py
@@ -11,8 +11,7 @@ router = APIRouter(prefix="/projects", tags=["Projects"])
 # Create a project (requires login)
 @router.post("/", response_model=schemas.ProjectResponse)
 def create_project(project: schemas.ProjectCreate, db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
-    project.user_id = current_user.id
-    db_project = models.Project(**project.dict())
+    db_project = models.Project(**project.dict(), user_id=current_user.id)
     db.add(db_project)
     db.commit()
     db.refresh(db_project)
